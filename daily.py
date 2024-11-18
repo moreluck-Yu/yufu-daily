9f1307c0032494f93c97819eb4a79b41c6d62bfc
moreluck-Yu <109788361+moreluck-Yu@users.noreply.github.com>
2024-11-18 20:29:23 +0800

    Update daily.py

--- daily.py
+++ daily.py
@@ -25,7 +25,7 @@ TG_CHAT_ID = os.environ['TG_CHAT_ID']
 # Get Weather Information: https://github.com/baichengzhou/weather.api/blob/master/src/main/resources/citycode-2019-08-23.json to find the city code
 # Shanghai 101020100
 # Hangzhou 101210101 by default
-WEATHER_CITY_CODE = os.environ.get('WEATHER_CITY_CODE', '')
+WEATHER_CITY_CODE = os.environ.get('WEATHER_CITY_CODE', '101180801')
 
 # -------------
 # Optional Settings. config in github secrets.
@@ -88,7 +88,7 @@ def make_pic_from_silicon(sentence):
     url = "https://api.siliconflow.cn/v1/images/generations"
     
     payload = {
-        "model": "Pro/black-forest-labs/FLUX.1-schnell",
+        "model": "black-forest-labs/FLUX.1-dev",
         "prompt": sentence,
         "image_size": "1024x1024"
     }
